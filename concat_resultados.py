6d546aacb47d8b35d943386473ad336f93c50c4e
Bruno Lucas Soares <brunolucassoares@gmail.com>
2023-08-27 19:56:04 -0300

    new file:   concat_resultados.py

---
+++ concat_resultados.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+# Defina o diretório onde estão os arquivos .xls
+diretorio = './jogos/'
+
+# Lista para armazenar os DataFrames de cada arquivo .xls
+dataframes = []
+
+# Percorre todos os arquivos no diretório
+for arquivo in os.listdir(diretorio):
+    if arquivo.endswith('.xlsx'):
+        caminho_arquivo = os.path.join(diretorio, arquivo)
+        df = pd.read_excel(caminho_arquivo, engine='openpyxl')
+        dataframes.append(df)
+
+# Concatena todos os DataFrames em um único DataFrame
+df_final = pd.concat(dataframes, ignore_index=True)
+
+# Exibe o DataFrame final
+print(df_final)
+print(df.describe().to_excel('estatistica_res.xlsx'))
+
